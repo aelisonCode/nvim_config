8188d359ec6ab3351b02a670e9850c3e4ced912c
Arnaud Elison <aelison@c1r8s13.42antananarivo.mg>
2026-04-13 16:00:27 +0300

    add: option for folding functions, blocks
    The options tell nvim the format to follow for the folding, and
    foldlevel tell nvim to keep all open at start

--- nvim/lua/configs/options.lua
+++ nvim/lua/configs/options.lua
@@ -2,13 +2,23 @@ local opt = vim.opt
 
 opt.number = true
 opt.relativenumber = true
+
 opt.shiftwidth = 8
 opt.tabstop = 8
 opt.expandtab = false
+
 opt.termguicolors = true
+
 opt.ignorecase = true
 opt.smartcase = true
+
 opt.clipboard = "unnamedplus"
 opt.undofile = true
 opt.confirm = true
 opt.swapfile = false
+
+-- Let wrap some functions, block of code: use keymap "za", "zc"
+opt.foldmethod = "syntax"
+opt.foldlevel = 99
+
+-- indent all: gg=G
