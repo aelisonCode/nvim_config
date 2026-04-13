ff89937bd26bde5ec7e8e1a90cb220b480c73597
Arnaud Elison <aelison@c1r8s13.42antananarivo.mg>
2026-04-13 15:13:23 +0300

    add: plugin for rust
    Let rust help me if there is completions, or syntaxe error in the code

---
+++ nvim/lua/plugins/rust.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5',
+    lazy = false,
+    config = function()
+      vim.g.rustaceanvim = {
+        server = {
+          on_attach = function(client, bufnr)
+            vim.keymap.set("n", "<leader>ca", function() vim.cmd.RustLsp('codeAction') end, { desc = "Rust Code Action", buffer = bufnr })
+          end,
+        },
+      }
+    end
+  },
+  -- Manage Cargo.toml dependencies
+  {
+    'saecki/crates.nvim',
+    tag = 'stable',
+    event = { "BufRead Cargo.toml" },
+    config = function()
+      require('crates').setup()
+    end,
+  },
+}
