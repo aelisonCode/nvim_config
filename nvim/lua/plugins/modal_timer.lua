200c4a834ee69b379c75ae75b6fa7cd9728dcfc5
Arnaud Elison <aelison@c1r8s14.42antananarivo.mg>
2026-04-16 11:04:13 +0300

    update: opt for modal_timer config
    Now it's possible to make autostart on nvim opening

--- nvim/lua/plugins/modal_timer.lua
+++ nvim/lua/plugins/modal_timer.lua
@@ -1,19 +1,20 @@
 return {
-  "aelisonCode/modal_timer.nvim",
-  dependencies = {
-    "aelisonCode/modal_reminder.nvim",
-  },
-  opts = {
-    times = { "09:30", "15:00"},
-    messages = { "Hey, time to take a break!", "You worked well, wanna go home?" },
-    keymaps = {
-      enabled = true,
-      start = "<leader>ts",
-      stop = "<leader>te",
-      status = "<leader>tc",
-    },
-  },
-  config = function(_, opts)
-    require("modal_timer").setup(opts)
-  end,
+	"aelisonCode/modal_timer.nvim",
+	dependencies = {
+		"aelisonCode/modal_reminder.nvim",
+	},
+	opts = {
+		autostart = true,
+		times = { "09:30", "15:00"},
+		messages = { "Hey, time to take a break!", "You worked well, wanna go home?" },
+		keymaps = {
+			enabled = true,
+			start = "<leader>ts",
+			stop = "<leader>te",
+			status = "<leader>tc",
+		},
+	},
+	config = function(_, opts)
+		require("modal_timer").setup(opts)
+	end,
 }
