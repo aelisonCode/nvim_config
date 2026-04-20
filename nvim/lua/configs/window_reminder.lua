af09ee82310daa8f5e94bca0f95e7b0dab4ffddc
Arnaud Elison <aelison@c1r8s15.42antananarivo.mg>
2026-04-20 12:30:12 +0300

    removing: plugin creation timer

--- nvim/lua/configs/window_reminder.lua
+++
@@ -1,8 +0,0 @@
-require("window_reminder").setup({
-	title = "Little reminder",
-	message = "Do not forget to take a break !",
-	keymaps = {
-		enabled = true,
-		launch = "<leader>tw"	-- Keymap to launch the window
-	},
-})
