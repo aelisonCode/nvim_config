442405ca7f02d0ac154032fa292aac360afa8972
Arnaud Elison <aelison@c1r8s15.42antananarivo.mg>
2026-04-21 07:44:12 +0300

    update lsp: following last versions of nvim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,46 +7,51 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
-			vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1 }) end, { desc = 'Go to previous error' })
-			vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1 }) end, { desc = 'Go to next error' })
-			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'LSP Code Actions' })
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
-			vim.keymap.set('n', 'gv', function()
+			-- keymaps
+			vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, { desc = "Go to previous error" })
+			vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, { desc = "Go to next error" })
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Actions" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+			vim.keymap.set("n", "gv", function()
 				vim.cmd("vsplit")
 				vim.lsp.buf.definition()
-			end, { desc = 'Go to definition in split' })
+			end, { desc = "Go to definition in split" })
 
 			vim.diagnostic.config({
-				float = {
-					border = "rounded",
-					source = "always",
-					header = "",
-					prefix = "",
-				},
+				float = { border = "rounded", source = "always", header = "", prefix = "" },
 			})
+
 			require("mason").setup()
 			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"clangd",
-					"pylsp",
-					"lua_ls",
-				},
+				ensure_installed = { "clangd", "pylsp", "lua_ls" },
 			})
 
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local servers = { "clangd", "pylsp", "lua_ls"}
 
-			for _, server in ipairs(servers) do
-				if vim.lsp.config then
-					vim.lsp.config(server, {
-						capabilities = capabilities,
-					})
-				else
-					require("lspconfig")[server].setup({
-						capabilities = capabilities,
-					})
-				end
-			end
+			vim.lsp.config("clangd", {
+				capabilities = capabilities,
+			})
+
+			vim.lsp.config("pylsp", {
+				capabilities = capabilities,
+			})
+
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						diagnostics = { globals = { "vim" } },
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = { enable = false },
+					},
+				},
+			})
+
+			vim.lsp.enable({ "clangd", "pylsp", "lua_ls" })
 		end,
 	},
 }
