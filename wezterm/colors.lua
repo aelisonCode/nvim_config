c44ff7f7151f8458ddcb8554e6ce386e7b780bc8
Arnaud Elison <aelison@c1r7s10.42antananarivo.mg>
2026-04-29 09:26:21 +0300

    update wezterm: let the image be responsive
    Now the image follow the window since
    Adding a variable to make the background more transparent in need
    Creating a simple variable to concatanate to source to simplify getting
    home path

--- wezterm/colors.lua
+++ wezterm/colors.lua
@@ -1,14 +1,19 @@
+-- local home = os.getenv("HOME")	Could be used to get the image path if need
+
 local colors = {
 	color_scheme = 'Tokyo Night',
 	window_background_opacity = 0.75,
 	background = {
 		{
 			source = {
-				File = '/home/aelison/Downloads/blured90.jpg',
+				File = "/home/aelison/.config/wezterm/background/tanjiro_blured20.jpg",
 			},
 			hsb = {
 				brightness = 0.7
 			},
+			width = "100%",
+			height = "100%",
+			opacity = 1,	-- Between 0 and 1 controle the transparency of the background image
 		},
 	},
 }
