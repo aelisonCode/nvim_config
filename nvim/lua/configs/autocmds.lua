f8a03261f0d70326c9738ee2dfd0be8f81d601f6
Arnaud Elison <aelison@c1r7s10.42antananarivo.mg>
2026-04-29 09:22:33 +0300

    add feature: adding autocmd to keep cursor last postion
    Let the cursor get back to his last position after opening a file inside
    a buffer

--- nvim/lua/configs/autocmds.lua
+++ nvim/lua/configs/autocmds.lua
@@ -1,26 +1,37 @@
+-- Keep track of cursor last position, and set cursor position directly there
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
 -- Shows the lsp error when cursor on it
 vim.opt.updatetime = 300
 vim.api.nvim_create_autocmd("CursorHold", {
-  callback = function()
-    local opts = {
-      focusable = false,
-      close_events = { "CursorMoved", "CursorMovedI", "BufLeave", "InsertEnter" },
-      border = "rounded",
-      source = "always",
-      prefix = " ",
-      scope = "cursor",
-    }
-    vim.diagnostic.open_float(nil, opts)
-  end,
+	callback = function()
+		local opts = {
+			focusable = false,
+			close_events = { "CursorMoved", "CursorMovedI", "BufLeave", "InsertEnter" },
+			border = "rounded",
+			source = "always",
+			prefix = " ",
+			scope = "cursor",
+		}
+		vim.diagnostic.open_float(nil, opts)
+	end,
 })
 
 -- Indent line called
 vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "*",
-  callback = function()
-    vim.api.nvim_set_hl(0, "IblIndent", { fg = "#3b4261", nocombine = true })
-    vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = "#bb9af7", bold = true })
-  end,
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_set_hl(0, "IblIndent", { fg = "#3b4261", nocombine = true })
+		vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = "#bb9af7", bold = true })
+	end,
 })
 
 vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = "#bb9af7", bold = true })
